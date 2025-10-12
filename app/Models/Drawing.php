a6cbf7f628947a213a33f18f95c03c8336266db2
cepsmohi <ceps.mohi@gmail.com>
2025-10-12 20:51:31 +0600

    Sun Oct 12 20:19:05 +06 2025

---
+++ app/Models/Drawing.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Drawing extends Model
+{
+    protected $fillable = [
+        'task_id',
+        'link',
+    ];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
