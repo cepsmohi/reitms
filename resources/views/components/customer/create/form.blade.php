dce0b23614e051fba6c48072fcb906ac5739500a
cepsmohi <ceps.mohi@gmail.com>
2025-11-05 20:36:54 +0600

    Tue Nov  4 17:17:56 +06 2025

--- resources/views/components/customer/create/form.blade.php
+++ resources/views/components/customer/create/form.blade.php
@@ -7,17 +7,11 @@
 >
     <x-form.inputwirelive
         name="customercode"
-        placeholder="Customer Code"
-        icon="tag"
-        hints="xxx-xxxxxx"
-        pattern="let v = this.value.replace(/\D/g, '').slice(0,14);
-                if (v.length > 13) this.value = v.replace(/(\d)(\d{2})(\d{6})(\d{2})(\d{2})(\d{1})/, '$1-$2-$3-$4-$5-$6');
-                else if (v.length > 11) this.value = v.replace(/(\d)(\d{2})(\d{6})(\d{2})(\d{1,2})/, '$1-$2-$3-$4-$5');
-                else if (v.length > 9) this.value = v.replace(/(\d)(\d{2})(\d{6})(\d{1,2})/, '$1-$2-$3-$4');
-                else if (v.length > 7) this.value = v.replace(/(\d)(\d{2})(\d{1,6})/, '$1-$2-$3');
-                else if (v.length > 3) this.value = v.replace(/(\d)(\d{1,2})/, '$1-$2');
-                else this.value = v;
-                this.dispatchEvent(new Event('input'));"
+        placeholder="Customer Code (if any)"
+        icon="link"
+        hints="x-xx-xxxxxx-xx-xx-x"
+        pattern="formatByPattern(this, [1,2,6,2,2,1])"
+        :required="false"
     />
     @if($customercode != '' && $customers != null)
         <div class="mb-4 w-full fcols gap-1">
--- resources/views/components/customer/details/forms/customercode.blade.php
+++ resources/views/components/customer/details/forms/customercode.blade.php
@@ -1,35 +1,18 @@
 @teleport('body')
-<div class="modalback bg-gray-900/90">
-    <div
-        class="modal bg-gray-100 p-4 rounded-3xl"
-    >
-        <x-ui.closeform wireclick="$toggle('editCustomerCodeForm')"/>
-        <form
-            wire:submit="updateCustomerCode"
-            id="editCustomerCodeForm"
-        >
-            <div class="fcol">
-                <div class="mb-7 stitle text-grad">Customer Code</div>
-                <x-form.inputwire
-                    name="customerCode"
-                    placeholder="#-##-######-##-##-#"
-                    icon="tag"
-                    pattern="let v = this.value.replace(/\D/g, '').slice(0,14);
-                    if (v.length > 13) this.value = v.replace(/(\d)(\d{2})(\d{6})(\d{2})(\d{2})(\d{1})/, '$1-$2-$3-$4-$5-$6');
-                    else if (v.length > 11) this.value = v.replace(/(\d)(\d{2})(\d{6})(\d{2})(\d{1,2})/, '$1-$2-$3-$4-$5');
-                    else if (v.length > 9) this.value = v.replace(/(\d)(\d{2})(\d{6})(\d{1,2})/, '$1-$2-$3-$4');
-                    else if (v.length > 7) this.value = v.replace(/(\d)(\d{2})(\d{1,6})/, '$1-$2-$3');
-                    else if (v.length > 3) this.value = v.replace(/(\d)(\d{1,2})/, '$1-$2');
-                    else this.value = v;
-                    this.dispatchEvent(new Event('input'));"
-                />
-                <x-form.submit-button
-                    form="editCustomerCodeForm"
-                    icon="refresh"
-                    tag="Update"
-                />
-            </div>
-        </form>
-    </div>
-</div>
+<x-form.form-modal
+    formCondition="editCustomerCodeForm"
+    :submitCondition="true"
+    submitFun="updateCustomerCode"
+    formId="editCustomerCodeForm"
+    formTitle="Edit Customer Code"
+>
+    <x-form.inputwire
+        name="customercode"
+        placeholder="Customer Code (if any)"
+        icon="link"
+        hints="x-xx-xxxxxx-xx-xx-x"
+        pattern="formatByPattern(this, [1,2,6,2,2,1])"
+        :required="false"
+    />
+</x-form.form-modal>
 @teleport('body')
